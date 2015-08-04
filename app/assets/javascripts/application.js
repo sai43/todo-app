114138cd3897b084984120fa0acdbd89f9943b3a
sai ch <chsai.btech@gmail.com>
2015-08-04 15:18:34 +0530

    added popover and tooltip

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,13 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+$(document).ready(function() {
+  $('.has-tooltip').tooltip();
+  $('.has-popover').popover({
+    trigger: 'hover'
+  });
+
+});
+
